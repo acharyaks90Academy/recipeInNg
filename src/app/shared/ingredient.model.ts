75d28361b2264fac9fe90b665e06083b61181dca
anil suryavanshi <thakurinfotech@gmail.com>
2017-08-06 17:56:54 +0530

    ingredient added model and list in view

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,11 @@
+export class Ingredient{
+    // public name:string;
+    // public amount:Number;
+
+    // constructor(name:string,amount:Number){
+    //     this.name = name;
+    //     this.amount = amount;
+
+    // }
+    constructor(public name:string,public amount:Number){};
+}
