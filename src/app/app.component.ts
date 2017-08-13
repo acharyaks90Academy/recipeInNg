743db8e9b33c2d4ae454b5e9396ffb2ded98d1ad
anil suryavanshi <thakurinfotech@gmail.com>
2017-08-13 16:42:25 +0530

    navigation by event and spit up code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  feratureSelected = "recipe";
+  onNavigate(feature:string){
+    this.feratureSelected = feature;
+  }
 }
