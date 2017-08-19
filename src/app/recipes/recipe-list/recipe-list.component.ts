2aa60ff8b23db32c34b9211c452ba99a620c1b84
anil suryavanshi <thakurinfotech@gmail.com>
2017-08-19 17:26:06 +0530

    where and how manager recipe data service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit,Output , EventEmitter} from '@angular/core';
-import { Recipe} from '../recipe.model'
+import { Recipe} from '../recipe.model';
+import {RecipeService} from '../recipe.service';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,13 +8,14 @@ import { Recipe} from '../recipe.model'
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes :Recipe[] = [
-    new Recipe('Test Recipe','Very tasty recipe','assets/Easy-Avocado-Shrimp-Salad-Recipe.jpg'),
-    new Recipe('Test Recipe','Very tasty recipe','assets/Easy-Avocado-Shrimp-Salad-Recipe.jpg')
-  ];
-  constructor() { }
+   recipes :Recipe[] ; //= [
+  //   new Recipe('Test Recipe','Very tasty recipe','assets/Easy-Avocado-Shrimp-Salad-Recipe.jpg'),
+  //   new Recipe('Test Recipe','Very tasty recipe','assets/Easy-Avocado-Shrimp-Salad-Recipe.jpg')
+  // ];
+  constructor(private recipeService:RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
   onRecipeSelected(recipe:Recipe){
     this.recipeWasSelected.emit(recipe);
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,11 @@
+import {Recipe}  from './recipe.model'
 export class RecipeService{
-    
+    recipes :Recipe[] = [
+        new Recipe('Test Recipe','Very tasty recipe','assets/Easy-Avocado-Shrimp-Salad-Recipe.jpg'),
+        new Recipe('Test Recipe','Very tasty recipe','assets/Easy-Avocado-Shrimp-Salad-Recipe.jpg')
+      ];
+
+      getRecipes(){
+          return this.recipes;
+      }
 }
