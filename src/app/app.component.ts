5073662eddb924aa80a255add36aad9b9be04003
anil suryavanshi <thakurinfotech@gmail.com>
2017-11-22 00:41:43 +0530

    Added auth services for sign up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
   feratureSelected = "recipe";
+  ngOnInit(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyCpdlBEjW8C07s2ZkUu2sr06QaPSdxBKmk",
+      authDomain: "ng-book-73259.firebaseapp.com"
+    });
+  }
   onNavigate(feature:string){
     this.feratureSelected = feature;
   }
