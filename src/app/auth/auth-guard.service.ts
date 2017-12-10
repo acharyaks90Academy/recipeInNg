2ba4dffcef02440d8cab1bb1bafc5bbf3eb0747b
anil suryavanshi <thakurinfotech@gmail.com>
2017-12-10 07:56:02 +0530

    Section 20 Auth guard service

---
+++ src/app/auth/auth-guard.service.ts
@@ -0,0 +1,16 @@
+
+import { CanActivate, ActivatedRoute, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
+import { AuthService } from "../auth/auth.service";
+import { Injectable } from '@angular/core';
+
+@Injectable()
+
+export class AuthGuard implements CanActivate {
+    constructor(private authservice: AuthService) {
+
+    }
+    canActivate(route: ActivatedRouteSnapshot, state:RouterStateSnapshot){
+        return this.authservice.isAuthenticated();
+    }
+    
+}
